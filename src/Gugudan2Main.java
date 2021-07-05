a26b435bca5e41bad5d537c38dc4f7623b7b6ee8
junghwankim <padd60@naver.com>
2021-07-05 16:59:00 +0900

    구구단 변형 구현 Gugudan2 = 사용자 입력한 값에 따라 2단부터 입력값까지 곱한 결과를 입력값단까지 수행하는 구구단 구현 Gugudan3 = 사용자가 두가지의 입력값을 주면 (예: 8,7) 2단부터 첫번째 입력값단까지 두번째 입력값을 곱한 결과를 출력하는 구구단 구현

---
+++ src/Gugudan2Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Gugudan2Main {
+	public static void main(String[] args) {
+		System.out.println("원하는 구구단은 ? : ");
+		Scanner scanner =new Scanner(System.in);
+		int number = scanner.nextInt();		
+		int[] result = new int[number];
+		for(int i = 2; i <= number; i++) {
+			for(int j = 0; j < result.length; j++) {
+				result[j] = i * (j+1);
+				System.out.println(result[j]);
+			}
+		}
+	}
+}
---
+++ src/Gugudan3Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Gugudan3Main {
+	public static void main(String[] args) {
+		System.out.println("원하는 구구단은 ? : ");
+		Scanner scanner =new Scanner(System.in);
+		String inputValue = scanner.nextLine();
+		String[] splitedValue  = inputValue.split(",");
+		int first = Integer.parseInt(splitedValue[0]);
+		int second = Integer.parseInt(splitedValue[1]);
+		
+		int[] result = new int[second];
+		for(int i=2; i <= first; i++) {
+			for(int j=0; j < second; j++) {
+				result[j] = i * (j + 1);
+				System.out.println(result[j]);
+			}
+		}
+	}
+}
