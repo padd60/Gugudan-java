37d9352b495b01aea8e4f989b449c590953db54a
junghwankim <padd60@naver.com>
2021-07-05 14:14:20 +0900

    매서드를 이용한 2단부터 9단까지 출력하는 구구단 구현

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -1,11 +1,37 @@
 public class Gugudan {
-	public static void main(String[] args) {
+	public static int[] caculate(int times) {
 		int[] result = new int[9];
-		for(int j = 2; j < 10; j++) {
-			for(int i = 0; i < result.length; i++) {
-					result[i] = j * (i+1);				
-					System.out.println(result[i]);
-			}
+		for(int i = 0; i < result.length; i++) {
+			result[i] = times * (i+1);
+		}
+		return result;
+	}
+	
+	public static void print(int[] result) {
+		for(int i=0; i < result.length; i++) {
+			System.out.println(result[i]);
+		}
+	}
+	
+	public static void main(String[] args) {
+		for(int i=2; i < 10; i++) {
+			int[] result = caculate(i);
+			print(result);
 		}
 	}
 }
+
+
+
+
+
+
+// 반복문을 통한 2단부터 9단까지 한번에 출력하
+//		int[] result = new int[9];
+//		for(int j = 2; j < 10; j++) {
+//			for(int i = 0; i < result.length; i++) {
+//					result[i] = j * (i+1);				
+//					System.out.println(result[i]);
+//			}
+//		}
+//	}
